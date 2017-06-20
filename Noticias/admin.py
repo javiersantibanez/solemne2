765c2ea0c48a93c034c5a643bcf2e741d649e56b
javier <javier.santibanez.93@gmail.com>
2017-06-19 23:23:27 -0400

    Definicion modelo Notica y Categoria

--- Noticias/admin.py
+++ Noticias/admin.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from Noticias.models import Noticia, Categoria
+
+class NoticiaAdmin (admin.ModelAdmin):
+	list_display = ('id','titular','categoria')
+
+
+class CategoriaAdmin (admin.ModelAdmin):
+	pass
+
+admin.site.register(Noticia,NoticiaAdmin)
+admin.site.register(Categoria,CategoriaAdmin)
 
 # Register your models here.
--- Noticias/models.py
+++ Noticias/models.py
@@ -3,4 +3,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=140)
+    sort_order = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Noticia(models.Model):
+    titular = models.CharField(max_length=140)
+    categoria = models.ForeignKey(Categoria)
+    description = models.TextField()
+    sort_order = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
